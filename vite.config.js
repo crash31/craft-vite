10b1a81a0838ccdfb4f8bd41da589700c2734b1c
Kyle Rodgers <kyle.rits.rodgers@gmail.com>
2021-09-08 16:43:25 -0400

    testing out pwa injection

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // import legacy from '@vitejs/plugin-legacy';
 import ViteRestart from 'vite-plugin-restart';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default ({ command }) => ({
@@ -21,6 +22,19 @@ export default ({ command }) => ({
       ],
     }),
     vue(),
+    VitePWA({
+      mode: 'development',
+      srcDir: 'src/js',
+      filename: 'sw.ts',
+      base: '/',
+      strategies: 'injectManifest',
+      includeAssets: [],
+      manifest: {
+        name: 'Craft Vite Inject Manifest',
+        short_name: 'Craft Vite',
+        theme_color: '#ffffff',
+      },
+    }),
   ],
   server: {
     host: '0.0.0.0',
